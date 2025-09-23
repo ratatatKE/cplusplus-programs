61c0c23924d9433cad0a212556b00360c08a44d5
ratatatKE <moses.ndeda@strathmore.edu>
2025-09-23 21:49:54 +0300

    made some changes to binary search

--- LearnCplusplusin24Hrs/LeetcodeGrind/BinarySearch/BinarySearchFindUpperBound.cpp
+++ LearnCplusplusin24Hrs/LeetcodeGrind/BinarySearch/BinarySearchFindUpperBound.cpp
@@ -6,42 +6,64 @@
 #include <iostream>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int search(vector<int> &nums, int target) {
-        //set the left and right boundaries
-        int left = 0, right = (int) nums.size();
+    int search(vector<int> &nums, int target)
+    {
+        // set the left and right boundaries
+        int left = 0, right = (int)nums.size();
 
-        while (left < right) {
+        while (left < right)
+        {
             int mid = left + (right - left) / 2;
 
-            if (nums[mid] <= target) {
+            if (nums[mid] <= target)
+            {
                 left = mid + 1;
-            } else {
+            }
+            else
+            {
                 right = mid;
             }
         }
 
-
-        if (left > 0 && nums[left - 1] == target) {
+        if (left > 0 && nums[left - 1] == target)
+        {
             return left - 1;
-        } else {
+        }
+        else
+        {
             return -1;
         }
     }
 };
 
-int main() {
-    vector<int> nums = {-1, 0, 3, 5, 9, 9, 9, 12}; //Upper bound is 6 as printed by the program i.e. the highest index of 9
+int main()
+{
+    vector<int> nums = {-1, 0, 3, 5, 9, 9, 9, 12}; // Upper bound is 6 as printed by the program i.e. the highest index of 9
     int target = 9;
 
-    // vector<int> nums = {-1, 0, 3, 5, 9, 12};
-    // int target = 2; //2 is not present it should return -1
+    vector<int> nums2 = {-1, 0, 3, 5, 9, 12};
+    int target2 = 2; // 2 is not present it should return -1
 
     Solution sol;
-    std::cout << "Input is " << std::endl;
-    for (int i = 0; i < nums.size(); i++) {
+
+    std::cout << "Input vector nums is " << std::endl;
+    for (int i = 0; i < nums.size(); i++)
+    {
         std::cout << nums[i] << std::endl;
     }
-    std::cout << "Index of solution is " << sol.search(nums, target) << std::endl;
+    std::cout << "Index of solution when input is as printed above for nums and target is " << target << " results to " << sol.search(nums, target) << std::endl;
+
+    std::cout << "--------------------------------------------------------------------------------------------------------------------------------" << std::endl;
+
+    vector<int> nums3 = {3};
+    int target3 = 3;
+    std::cout << "Input vector nums3 is " << std::endl;
+    for (int i = 0; i < nums3.size(); i++)
+    {
+        std::cout << nums3[i] << std::endl;
+    }
+    std::cout << "Index of solution when input is as printed above for nums3 and target3 is " << target3 << " results to " << sol.search(nums3, target3) << std::endl;
 }
--- LearnCplusplusin24Hrs/LeetcodeGrind/BinarySearch/BinarySearchVariationsTheoryBoundaryVsExactMatch.cpp
+++ LearnCplusplusin24Hrs/LeetcodeGrind/BinarySearch/BinarySearchVariationsTheoryBoundaryVsExactMatch.cpp
@@ -17,11 +17,19 @@ public:
         int right = n; //Note that right =n and NOT n-1, we might insert at the VERY end
 
         // WHY right = n? Because for array {1, 3, 5, 7, 9} which has a size of 5, 5 is out of bounds when indexing but it is valid for insertion as it is at the VERY end
-        
-        while (left < right){
-            int mid = left + (right - left)/2;
 
+        while (left < right){
+            int mid = left + (right - left)/2; //will do a floor division in C++
+            
+            if(arr[mid] < target){
+                left = mid + 1; //Boundary is to the right of mid
+            }else{
+                right = mid; // We have already checked mid. So right boundary MUST be at mid, so that the next iteration we check mid < 1 since our while loop is using < when comparing left and right
+            }
         }
+
+        //After breaking out of the while loop completely
+        return left; // left == right, this is our boundary position. Confirm if left == right == mid while debugging. UPDATE: Confirmed after debugging, left == mid == right
     }
 };
 
@@ -31,5 +39,7 @@ int main()
     Solution sol;
 
     int arr1[] = {1, 3, 5, 7, 9};
-    cout << "During the test for findInsertionPoint() with array [1,3,5,7,9], we expected position 3 and we got -> " << sol.findInsertionPoint(arr1, 5, 6) << endl;
+    int arr2[] = {3};
+    // cout << "During the test for findInsertionPoint() with array [1,3,5,7,9], we expected position 3 and we got -> " << sol.findInsertionPoint(arr1, 5, 6) << endl;
+    cout << "During the test for findInsertionPoint() with array [3], we expected position 0 and we got -> " << sol.findInsertionPoint(arr2, 1, 3) << endl;
 }
